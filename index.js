8af8e816ee72cf3c18da69dae666ec10bba1248c
wahidgoni17 <imwahidgoni17@gmail.com>
2023-05-18 21:16:44 +0600

    gallery data added

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const piccollections = client.db("toyhub").collection("gallery")
+    // gallery section
+    app.get("/gallery", async(req, res)=>{
+        const cursor = piccollections.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
